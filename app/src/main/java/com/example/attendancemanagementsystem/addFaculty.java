40f8c359cf652ecde6c5168a4ee5e20539e3de26
Kalyan1003 <143153921+Kalyan1003@users.noreply.github.com>
2024-03-04 20:47:23 -0600

    Kalyan Kumar Thatikonda AddFaculty page

---
+++ app/src/main/java/com/example/attendancemanagementsystem/addFaculty.java
@@ -0,0 +1,14 @@
+package com.example.attendancemanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addFaculty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_faculty);
+    }
+}
--- app/src/main/java/com/example/attendancemanagementsystem/index.java
+++ app/src/main/java/com/example/attendancemanagementsystem/index.java
@@ -2,7 +2,10 @@ package com.example.attendancemanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class index extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class index extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        Button addfacultyBTN = findViewById(R.id.addfacultyBTN);
+        addfacultyBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickAddFacultyBTN(v);
+            }
+        });
+    }
+    public void onClickAddFacultyBTN(View v){
+        Intent intent = new Intent(this, addFaculty.class);
+        startActivity(intent);
     }
 }
