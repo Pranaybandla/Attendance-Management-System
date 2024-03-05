970dbd80b93ef280422275a724ca9e15bef5d54f
Meghanaankam16 <143150657+Meghanaankam16@users.noreply.github.com>
2024-03-04 20:21:22 -0600

    Meghana Ankam index page

--- app/src/main/java/com/example/attendancemanagementsystem/login.java
+++ app/src/main/java/com/example/attendancemanagementsystem/login.java
@@ -13,6 +13,17 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button loginBTN = findViewById(R.id.loginBTN);
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickLoginBTN(v);
+            }
+        });
 
+    }
+    public void onClickLoginBTN(View v){
+        Intent in = new Intent(this, index.class);
+        startActivity(in);
     }
 }
